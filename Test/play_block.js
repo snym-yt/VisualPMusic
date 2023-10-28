0630a23c1b0731c1d67e16b3662b65d31dfc5790
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-10-28 10:22:17 +0900

    github pages使うようにフォルダ作成

---
+++ Test/play_block.js
@@ -0,0 +1,24 @@
+[{
+  "type": "play",
+  "message0": "play %1 Note No. %2 during %3",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "Note No.",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "during",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": 230,
+  "tooltip": "ノートナンバーの音をduringの間流す",
+  "helpUrl": ""
+}]
