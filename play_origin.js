5e4e27dccf15a86c334e7b2ccc9550d6a00e220e
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-10-30 17:39:21 +0900

    ブロックのいらないエクスポートファイルを削除

--- play_origin.js
+++
@@ -1,8 +0,0 @@
-javascript.javascriptGenerator.forBlock['play'] = function(block, generator) {
-  var value_note_no_ = generator.valueToCode(block, 'Note No.', javascript.Order.ATOMIC);
-  var value_during = generator.valueToCode(block, 'during', javascript.Order.ATOMIC);
-  // TODO: Assemble javascript into code variable.
-  var code = 'play(' + value_note_no_ + ', ' + value_during + ');';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.javascript.ORDER_NONE];
-};
