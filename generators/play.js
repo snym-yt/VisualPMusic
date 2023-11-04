4f565eb20397730ceb4c2c597ada9d9c378fd21b
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-11-04 20:30:39 +0900

    index.htmlのタブ修正，randomブロックを試しに追加，動作せず

--- generators/play.js
+++ generators/play.js
@@ -2,6 +2,6 @@ javascript.javascriptGenerator.forBlock['play'] = function(block, generator) {
   var value_note_no_ = generator.valueToCode(block, 'Note No.', javascript.Order.ATOMIC);
   var value_during = generator.valueToCode(block, 'during', javascript.Order.ATOMIC);
   // TODO: Assemble javascript into code variable.
-  var code = '...\n';
+  var code = 'play(' + value_note_no_ + ', ' + value_during + ');\n';
   return code;
 };
---
+++ generators/random.js
@@ -0,0 +1,8 @@
+javascript.javascriptGenerator.forBlock['random'] = function(block, generator) {
+  var value_min = generator.valueToCode(block, 'min', javascript.Order.ATOMIC);
+  var value_max = generator.valueToCode(block, 'max', javascript.Order.ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = 'Math.floor( Math.random() * (' + value_max + ' - ' + value_min + ' + 1 )) + ' +  value_min;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.javascript.ORDER_NONE];
+};
