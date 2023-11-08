845a821de786917f8eeaa0d3dd7397e7a4f2292b
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-11-08 20:29:44 +0900

    loopの元となるものとwhileを追加，Musicのカテゴリを赤色に(中身はまだ)

--- generators/random.js
+++ generators/random.js
@@ -2,7 +2,7 @@ Blockly.JavaScript['random'] = function(block, generator) {
   var value_min = generator.valueToCode(block, 'min', javascript.Order.ATOMIC);
   var value_max = generator.valueToCode(block, 'max', javascript.Order.ATOMIC);
   // TODO: Assemble javascript into code variable.
-  var code = 'Math.floor( Math.random() * (' + value_max + ' - ' + value_min + ' + 1 )) + ' +  value_min;
+  var code = 'Math.floor( Math.random() * (' + value_max + ' - ' + value_min + ' + 1 )) + ' +  value_min + "\n";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.javascript.ORDER_NONE];
 };
