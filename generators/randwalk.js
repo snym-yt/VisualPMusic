ef1207c607e4227fd8649a02e9ae8d7a572bb1b4
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-11-20 15:22:02 +0900

    weibulとrandwalkもPMusicのコードを生成

--- generators/randwalk.js
+++ generators/randwalk.js
@@ -2,6 +2,6 @@ Blockly.JavaScript['randwalk'] = function(block, generator) {
   var number_num_of_note = block.getFieldValue('num_of_note');
   var number_start_noteno = block.getFieldValue('start_noteNo');
   // TODO: Assemble javascript into code variable.
-  var code = 'randwalk\n';
+  var code = 'randwalk(' + number_num_of_note + ', ' + number_start_noteno + ');\n';
   return code;
 };
--- generators/weibul.js
+++ generators/weibul.js
@@ -3,6 +3,6 @@ Blockly.JavaScript['weibul'] = function(block, generator) {
   var value_scale = Blockly.JavaScript.valueToCode(block, 'scale', Blockly.JavaScript.ORDER_ATOMIC);
   // var value_position = Blockly.JavaScript.valueToCode(block, 'position', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble javascript into code variable.
-  var code = value_shape + value_scale + '...\n';
+  var code = 'weibul(' + value_shape + ', ' + value_scale + ');\n';
   return code;
 };
