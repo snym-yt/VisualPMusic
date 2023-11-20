782227b1995a1ebbda9c8081a0823c22716248af
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-11-20 15:16:55 +0900

    gaussブロックの生成コードをPMusicに準拠

--- generators/gauss2.js
+++ generators/gauss2.js
@@ -3,6 +3,6 @@ Blockly.JavaScript['gauss'] = function(block, generator) {
   var number_ave_of_noteno = block.getFieldValue('ave_of_noteNo');
   var number_var = block.getFieldValue('var');
   // TODO: Assemble javascript into code variable.
-  var code = number_num_of_note + number_ave_of_noteno + number_var + '\n';
+  var code = 'gauss(' + number_num_of_note + ', ' + number_ave_of_noteno + ', ' + number_var + ');\n';
   return code;
 };
