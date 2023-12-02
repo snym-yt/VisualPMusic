4863889d5cf37ae809189cf058bbf3fc5d168ced
Sonoyama Yuto <rokokoromayuge@icloud.com>
2023-12-02 21:10:25 +0900

    Markdown内容を他ファイルにしようとしたが，ブラウザのセキュリティ的な面で辞めた

--- Instruction/inst_markdown.js
+++ Instruction/inst_markdown.js
@@ -3,29 +3,127 @@
 
     //`と`の間に、表示したいマークダウンを左詰めで記述する。
     var text = `
-##### aaa
-*Lorem ipsum* dolor sit amet, consectetur adipiscing elit. 
-Curabitur vel quam sit amet ligula volutpat condimentum. 
-Sed eu orci vel libero varius suscipit. 
-Nullam condimentum dolor nec efficitur semper. 
-Nullam condimentum dolor nec efficitur semper. 
-Integer eget dui vitae quam gravida iaculis.
-- aa
-    - aaaa
-- aa
+# VisualPMusic
 
-a
-a
-a
-aa
-a
+### MIDIノートナンバー
 
-あ
+PMusicでは鍵盤を数字で表す[MIDIノートナンバー](https://www.asahi-net.or.jp/~hb9t-ktd/music/Japan/Research/DTM/freq_map.html)を使用している．
+
+### 構文
+
+##### (1)変数束縛
+
+##### (2)繰り返し
+
+次のように入れ子構造にすることも可能．
+
+loop文は本文を書くところに変数束縛を使用するとエラーが発生して終了してしまっていることに注意．
+
+また．本リポジトリでビジュアルプログラミング化する際にBlocklyに元から組み込まれているものを流用するため，"loop"は"repeat"として表されている．
+
+##### (4)条件分岐
+
+
+##### (5)配列
+
+配列の要素は型が一致していなくてもよい．
+
+
+[Blocklyの配列のWiki](https://github.com/google/blockly/wiki/Lists)
+「create empty list」，「create list with」，「length of」のみ実装．
+
+
+
+### 組み込み関数(確率音楽)：確率音楽関数
+
+##### (1)gauss
+
+引数にとったパラメータから[ガウス分布](https://ja.wikipedia.org/wiki/%E6%AD%A3%E8%A6%8F%E5%88%86%E5%B8%83)を作成し，そのガウス分布からランダムに音を生成する．
+
+
+##### (2)weibul
+
+引数に取ったパラメータから[ワイブル分布](https://poncotty.com/2020/03/28/%E3%83%AF%E3%82%A4%E3%83%96%E3%83%AB%E5%88%86%E5%B8%83/)を作成し，そのワイブル分布からランダムに音を生成する．他の確率音楽関数と違って，一度に生成できる音の数は１のみ．引数が4つは多いと判断したため．
+
+
+
+位置パラメータの値を1~5に制限しているのは，weibul関数の知識がない人が位置パラメータの値を大きく取り過ぎてノートナンバーが以上に高くなるのを防ぐため．
+
+##### (3)randwalk
+
+ガウス分布から音高の変化値を抽出し，現在の音高に加えていく．
+
+
+
+### 組み込み関数(音生成)
+
+##### (1)play
+
+
+### 組み込み関数(その他)
+
+##### (1)len
+
+文字列の長さや配列の大きさを返す．
+
+
+
+##### (2)first
+
+配列の先頭の要素を取得する．
+
+
+
+##### (3)last
+
+配列の最後尾の要素を取得する．
+
+
+
+##### (4)rest
+
+与えられた配列の最初の要素を取り除いた配列を返す．
+返しているのは新しく作成した残り要素の配列なので，引数に渡された配列自体は変化していない．
 
 
 
 
-`
+##### (5)push
+
+配列の最後尾に新しい要素を追加する．
+この組み込み関数においても，返してるのは新しく作成した配列なので，引数に渡された配列自体は変化していない．
+
+PMusicにおいて配列は不変としている．
+
+
+##### (6)puts
+
+与えられた引数を改行つきで標準出力する．
+引数の個数は可変であり，それぞれを別の行に表示する．
+標準出力を行うだけで値は生成しない．
+
+
+
+
+### 型付け
+
+動的型付け
+
+### データ型
+- int型
+- float型
+- string型
+- boolean型
+- リスト型(配列)
+- ハッシュ型
+
+### Process Virtual Machine
+- スタックマシン
+
+[スタックマシンとレジスタマシン](http://fe0km.blog.fc2.com/blog-entry-106.html)
+
+
+`;
     var html = marked(text);
     document.getElementById('markdown_preview').innerHTML = html;
   })
